07e6f03f40292b32846a47918194b6b13eae6590
samitariq12 <samitariq40@gmail.com>
2020-07-18 20:41:58 +0500

    this is a hello

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello | Muhammad Sami Tariq <br>Another Hello</br>
+          this is a hello
         </p>
         <a
           className="App-link"
