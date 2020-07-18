1f1fe4b79040b0b2eebc619131e562533a6a8708
samitariq12 <samitariq40@gmail.com>
2020-07-18 15:55:33 +0500

    App.js again changed

--- src/App.js
+++ src/App.js
@@ -7,9 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello | Muhammad Sami Tariq</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello | Muhammad Sami Tariq
         </p>
         <a
           className="App-link"
